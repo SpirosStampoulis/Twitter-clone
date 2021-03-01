480c8f90de2b2b6f66fc2963685481dcaf9fdf43
Spiros Stampoulis <sstaboulis@gmail.com>
2021-03-01 21:31:16 +0100

    add tweet box

---
+++ src/TweetBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TweetBox.css";
+import { Avatar, Button, Input } from "@material-ui/core";
+
+function TweetBox() {
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <input placeholder="What's happening?" type="text" />
+        </div>
+        <Button type="submit">Tweet</Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
