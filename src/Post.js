20bbfa2a2e97571bb94660140373fe21464ef0a7
Spiros Stampoulis <sstaboulis@gmail.com>
2021-03-01 21:29:53 +0100

    add post

---
+++ src/Post.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import "./Post.css";
+import { Avatar } from "@material-ui/core";
+
+function Post({
+  displayName,
+  username,
+  verified,
+  timestamp,
+  text,
+  image,
+  avatar,
+}) {
+  return (
+    <div className="post">
+      <div className="post__avatar">
+        <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>
+              Rafeh Qazi{" "}
+              <span className="post__headerSpecial">
+                <VerifiedUserIcon className="post__badge" /> @cleverqazi · 3h
+              </span>
+            </h3>
+          </div>
+          <div className="post__headerDescription">
+            I challenge you to build a Twitter Clone with React!!!
+          </div>
+        </div>
+        <img
+          src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif"
+          alt=""
+        />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
